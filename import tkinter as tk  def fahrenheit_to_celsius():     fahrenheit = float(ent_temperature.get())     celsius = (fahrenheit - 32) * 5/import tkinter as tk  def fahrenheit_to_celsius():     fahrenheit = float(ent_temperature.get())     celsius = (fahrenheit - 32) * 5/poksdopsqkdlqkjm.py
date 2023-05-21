9eb4638de2da829cc2eeb61db8ddd29735fb7317
AzizLabidiGoMyCode <127601212+AzizLabidiGoMyCode@users.noreply.github.com>
2023-05-21 12:03:25 +0200

    Create poksdopsqkdlqkjm.py

---
+++ import tkinter as tk  def fahrenheit_to_celsius():     fahrenheit = float(ent_temperature.get())     celsius = (fahrenheit - 32) * 5/import tkinter as tk  def fahrenheit_to_celsius():     fahrenheit = float(ent_temperature.get())     celsius = (fahrenheit - 32) * 5/poksdopsqkdlqkjm.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def fahrenheit_to_celsius():
+    fahrenheit = float(ent_temperature.get())
+    celsius = (fahrenheit - 32) * 5/9
+    lbl_result["text"] = f"{celsius} \N{DEGREE CELSIUS}"
+
+window = tk.Tk()
+window.title("Temperature Converter")
+window.resizable(width=False, height=False)
+
+ent_temperature = tk.Entry(window, width=10)
+ent_temperature.grid(row=0, column=0)
+
+btn_convert = tk.Button(window, text="Convert", command=fahrenheit_to_celsius)
+btn_convert.grid(row=0, column=1)
+
+lbl_result = tk.Label(window)
+lbl_result.grid(row=0, column=2)
+
+window.mainloop()
