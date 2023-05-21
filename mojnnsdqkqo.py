19e6825b48b7963bfa4b18dad0781a6c1f3c8f91
AzizLabidiGoMyCode <127601212+AzizLabidiGoMyCode@users.noreply.github.com>
2023-05-21 11:00:58 +0200

    Create mojnnsdqkqo.py

---
+++ mojnnsdqkqo.py
@@ -0,0 +1,39 @@
+#mylist=[14, "hello", 967]
+#mylist[6] 
+# corrected code
+mylist=[14, "hello", 967]
+mylist[2]
+#import Pandas
+#mport NumPy    
+# install the packages with pip
+#Print"python errors"
+# a corrected code 
+print("Python errors")
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+#mydictionnary["True"]
+mydictionnary[True]
+
+#i=14
+#while i<78:
+#print(i)
+#i+=1
+i=14
+while i<78:
+    print(i)
+i+=1
+
+
+it=iter([1,2,3])
+next(it)
+next(it)
+#next(it)
+#next(it)
+
+#'15'+15
+a = 15+15
+
+#int("python")
+int(python)
+#python
+python = "python"
+#x=19/0 cannot devide by zero   
